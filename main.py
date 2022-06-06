4436f99d36c83c1d17546246b2f4b06661826c50
Clamorae <quentin.trombini@utbm.fr>
2022-06-06 15:25:22 +0200

    starting the constraint, soon all respected

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from telnetlib import DO
+from traceback import print_tb
 import pandas as pd
 import numpy as np
 import random as rd
 
 intervenants = 4
 missions = 45
+intervenantsCSV = pd.read_csv("./Instances/45-4/Intervenants.csv", header=None)
+missionsCSV = pd.read_csv("./Instances/45-4/Missions.csv", header=None)
+distancesCSV = pd.read_csv("./Instances/45-4/Distances.csv", header=None)
 
+
+#-----------------genetic----------------------------------------------------------------------------------
 def genetic(solutions,missions):
     newGeneration = np.empty((20,missions))
 
@@ -14,7 +19,6 @@ def genetic(solutions,missions):
         b = (4*i) + 1
         c = (4*i) + 2
         d = (4*i) + 3
-        print(a,b,c,d)
         #order crossover
         x = rd.randrange(1,missions-1)
         y = rd.randrange(1,missions-1)
@@ -56,22 +60,67 @@ def genetic(solutions,missions):
             else:
                 newGeneration[d,i] = solutions[c,i]
                 newGeneration[c,i] = solutions[d,i]
-        #ANCHOR maybe more mutation type
+    
+    #ANCHOR maybe more mutation type
+    return newGeneration
         
-    for i in range (20):
-        print("[", end = '')
-        for j in range (missions):
-            print(newGeneration[i,j],",", end = '')
-        print("],")
+   
+#-----------------compute_score----------------------------------------------------------------------------------
+def compute_score(solution, missions, intervenants, intervenantsCSV, missionsCSV, distancesCSV):
+    score = 0.0
+    firstDailyMission = np.zeros(intervenants)
+    lastMission = np.zeros(intervenants)
+    dailyWorkTime = np.zeros((intervenants,5))
+    totWork = np.zeros(intervenants)
+
+    
+    for i in range (missions):
+        # check for the competences affectation 
+        if intervenantsCSV.values[int(solution[i])][1] != missionsCSV.values[i][4]:
+            score+= 999.9
+        
+        # check if there is less than 12h between the first and the last mission
+        if firstDailyMission[int(solution[i])]!=0:
+            if missionsCSV.values[int(firstDailyMission[int(solution[i])])][1]!=missionsCSV.values[i][1]:
+                if missionsCSV.values[int(lastMission[int(solution[i])])][1] - missionsCSV.values[int(firstDailyMission[int(solution[i])])][1] >720:
+                    score+= 999.9
+                firstDailyMission[int(solution[i])]=i
+        else:
+            firstDailyMission[int(solution[i])]=i
+            lastMission[int(solution[i])]=i
+        
+        dailyWorkTime[int(solution[i])][int(missionsCSV.values[i][1])-1] += int(missionsCSV.values[i][3]) - int(missionsCSV.values[i][2])
+
+    for i in range (intervenants):
+        for j in range(5):
+            if (dailyWorkTime[i][j] > 360 and int(intervenantsCSV.values[i][3])==24) or (dailyWorkTime[i][j] > 480 and int(intervenantsCSV.values[i][3])==35):
+                score+=999.9
+            totWork[i]+=dailyWorkTime[i][j]
+
+        if totWork[i] > int(intervenantsCSV.values[i][3])*60 + 600:
+            score +=999.9
+                  
+
+
+    return score
 
 #-----------------MAIN-----------------------------------------------------------------------------------------
-solutions  = np.empty((20,missions))
+solutions  = np.zeros((20,missions))
+solScore = np.zeros(20)
 for i in range (20):
-    print("[", end = '')
+#    print("[", end = '')
     for j in range (missions):
         solutions[i,j] = rd.randrange(0,intervenants)
-        print(solutions[i,j],",", end = '')
-    print("],")
+#        print(solutions[i,j],",", end = '')
+#    print("],")
+
 solutions = genetic(solutions,missions)
+for i in range (20):
+    solScore[i] = compute_score(solutions[i],missions,intervenants, intervenantsCSV, missionsCSV, distancesCSV)
+print(solScore)
 
 
+#TODO finir contrainte + score
+#TODO loop sur newgen
+#TODO mutation
+#TODO final comput
